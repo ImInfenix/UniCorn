dc13113fd4383834b82e61d3cd41c6706d202804
ImInfenix <duncan.sourdin@pm.me>
2022-01-21 00:45:10 +0100

    Setup UniCorn backbone

---
+++ Runtime/Scripts/Core/ServiceProvider.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace UniCorn.Core
+{
+    public static class ServiceProvider
+    {
+        private static readonly Dictionary<Type, IService> _services = new Dictionary<Type, IService>();
+
+        static ServiceProvider()
+        {
+        }
+
+        public static void RegisterService<T>(T service) where T : IService
+        {
+            _services.Add(typeof(T), service);
+            service.Initialize();
+        }
+
+        public static void UnregisterService<T>() where T : IService
+        {
+            _services.Remove(typeof(T));
+        }
+
+        public static void GetService<T>(out T service) where T : IService
+        {
+            service = (T) _services[typeof(T)];
+        }
+    }
+}
