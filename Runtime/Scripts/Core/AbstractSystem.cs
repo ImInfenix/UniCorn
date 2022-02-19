79a2dd3daaf019edf2a0bf41b1f4e21961e4d8a5
ImInfenix <duncan.sourdin@pm.me>
2022-02-19 19:44:51 +0100

    Created AbstractSystem

---
+++ Runtime/Scripts/Core/AbstractSystem.cs
@@ -0,0 +1,15 @@
+﻿namespace UniCorn.Core
+{
+    public abstract class AbstractSystem<LAYOUT_TYPE>
+    {
+        protected LAYOUT_TYPE _layout;
+
+        public void SetLayout(LAYOUT_TYPE layout)
+        {
+            _layout = layout;
+            OnLayoutInitialized();
+        }
+
+        protected abstract void OnLayoutInitialized();
+    }
+}
