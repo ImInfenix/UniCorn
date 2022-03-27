cc2c42f163612639d836088abba070cbcaf66495
ImInfenix <duncan.sourdin@pm.me>
2022-03-27 15:01:46 +0200

    Created abstract layout

---
+++ Runtime/Scripts/Core/AbstractLayout.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace UniCorn.Core
+{
+    public abstract class AbstractLayout : MonoBehaviour
+    {
+        public void Show()
+        {
+            gameObject.SetActive(true);
+        }
+
+        public void Hide()
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
