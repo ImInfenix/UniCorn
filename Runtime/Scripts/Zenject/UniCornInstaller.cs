e71b628e123d3e4b072cab13ec9857024246cc35
ImInfenix <duncan.sourdin@pm.me>
2022-03-27 15:02:54 +0200

    Updated UniCorn installer to install the navigation system

--- Runtime/Scripts/Zenject/UniCornInstaller.cs
+++ Runtime/Scripts/Zenject/UniCornInstaller.cs
@@ -1,23 +1,27 @@
 using UniCorn.Core;
 using UniCorn.Input;
 using UniCorn.Localization;
+using UniCorn.Navigation;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using Zenject;
 
 namespace UniCorn.Zenject
 {
     public class UniCornInstaller : MonoInstaller<UniCornInstaller>
     {
-        [SerializeField] private InputDefinition _inputDefinition;
+        [SerializeField] private InputActionAsset _inputActionAsset;
         [SerializeField] private LocalizationSettings _localizationSettings;
 
         public override void InstallBindings()
         {
-            Container.Bind<InputDefinition>().FromInstance(_inputDefinition).AsSingle();
             Container.Bind<LocalizationSettings>().FromInstance(_localizationSettings).AsSingle();
 
             Container.BindInterfacesAndSelfTo<InputService>().AsSingle();
+            Container.BindInterfacesAndSelfTo<NavigationService>().AsSingle();
             Container.BindInterfacesAndSelfTo<TranslationService>().AsSingle();
+            
+            Container.Bind(typeof(IInputActionCollection), typeof(IInputActionCollection2)).FromInstance(_inputActionAsset).AsSingle();
             
             Container.Bind<UniCornMonoBehaviour>().FromNewComponentOnNewGameObject().AsSingle();
         }
