17dc1a7f26597ad7ae41e2454adf23fac589e2ec
ImInfenix <duncan.sourdin@pm.me>
2022-03-27 14:59:40 +0200

    Created the navigation system

---
+++ Runtime/Scripts/Navigation/NavigationService.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using UniCorn.Input;
+using UniCorn.Utils;
+using UnityEngine.InputSystem;
+
+namespace UniCorn.Navigation
+{
+    public class NavigationService
+    {
+        private readonly List<InteractableItem> _registeredItems = new();
+        private readonly Dictionary<string, InputDefinition> _registeredInputDefinitions = new();
+
+        public void Register(InteractableItem interactableItem)
+        {
+            _registeredItems.Add(interactableItem);
+
+            foreach (InputDefinition inputDefinition in interactableItem.InputDefinitions)
+            {
+                _registeredInputDefinitions.AddIfDoesntExist(inputDefinition.name, inputDefinition);
+            }
+        }
+
+        public void Unregister(InteractableItem interactableItem)
+        {
+            _registeredItems.Remove(interactableItem);
+        }
+
+        public void OnInputAction(InputAction.CallbackContext callbackContext, InputDefinition inputDefinition)
+        {
+            foreach (InteractableItem interactableItem in _registeredItems)
+            {
+                if (!interactableItem.DoesListenToInputDefinition(inputDefinition))
+                {
+                    continue;
+                }
+
+                switch (callbackContext.phase)
+                {
+                    case InputActionPhase.Started:
+                        interactableItem.OnItemPressed();
+                        break;
+                    case InputActionPhase.Performed:
+                        break;
+                    case InputActionPhase.Canceled:
+                        interactableItem.OnItemReleased();
+                        break;
+                }
+
+                break;
+            }
+        }
+    }
+}
