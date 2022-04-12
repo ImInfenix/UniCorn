b9c09183a4122c115ac9ea5326c329c29e6da2bb
ImInfenix <46200328+ImInfenix@users.noreply.github.com>
2022-04-12 20:41:12 +0200

    Created Grid structure and tests

---
+++ Runtime/Scripts/Structures/Grid.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using UnityEngine;
+
+namespace UniCorn.Structures
+{
+    public class Grid<T>
+    {
+        public int Width { get; }
+        public int Height { get; }
+        private T[,] GridContent { get; }
+
+        public T this[Vector2Int position]
+        {
+            get => this[position.x, position.y];
+            set => this[position.x, position.y] = value;
+        }
+
+        public T this[int x, int y]
+        {
+            get => GridContent[x, y];
+            set => GridContent[x, y] = value;
+        }
+
+        public Grid(int width, int height)
+        {
+            if (width <= 0)
+            {
+                throw new ArgumentException($"The parameter {width} must be higher than 0");
+            }
+            
+            if (height <= 0)
+            {
+                throw new ArgumentException($"The parameter {height} must be higher than 0");
+            }
+            
+            Width = width;
+            Height = height;
+
+            GridContent = new T[width, height];
+
+            for (int i = 0; i < width; i++)
+            {
+                for (int j = 0; j < height; j++)
+                {
+                    GridContent[i, j] = default;
+                }
+            }
+        }
+
+        public bool IsPositionInGrid(Vector2Int position) => IsPositionInGrid(position.x, position.y);
+        public bool IsPositionInGrid(int x, int y) => x >= 0 && x < Width && y >= 0 && y < Height;
+    }
+}
---
+++ Tests/Runtime/Structures/GridTest.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using NUnit.Framework;
+using UniCorn.Structures;
+using UnityEngine;
+
+namespace UniCorn.Tests.Structures
+{
+    public class GridTest
+    {
+        private const int HEIGHT = 10;
+        private const int WIDTH = 10;
+
+        private Grid<int> _grid;
+
+        [SetUp]
+        public void Setup()
+        {
+            _grid = new Grid<int>(WIDTH, HEIGHT);
+        }
+
+        [Test]
+        public void ConstructorParameters()
+        {
+            Assert.AreEqual(WIDTH, _grid.Width);
+            Assert.AreEqual(HEIGHT, _grid.Height);
+        }
+
+        [Test]
+        public void ConstructorExceptions()
+        {
+            ArgumentException widthException = null;
+            ArgumentException heightException = null;
+
+            try
+            {
+                var unusedGrid = new Grid<int>(0, 1);
+            }
+            catch (ArgumentException e)
+            {
+                widthException = e;
+            }
+
+            try
+            {
+                var unusedGrid = new Grid<int>(1, 0);
+            }
+            catch (ArgumentException e)
+            {
+                heightException = e;
+            }
+
+            Assert.IsNotNull(widthException);
+            Assert.IsNotNull(heightException);
+        }
+
+        [Test]
+        public void GetterDefaultValue()
+        {
+            Assert.AreEqual(_grid[0, 0], 0);
+            Assert.AreEqual(_grid[Vector2Int.zero], 0);
+        }
+
+        [Test]
+        public void GetterAndSetterModifiedValue()
+        {
+            _grid[1, 1] = 8;
+            Assert.AreEqual(_grid[1, 1], 8);
+            _grid[Vector2Int.zero] = 5;
+            Assert.AreEqual(_grid[Vector2Int.zero], 5);
+        }
+
+        [Test]
+        public void IsPositionInGrid()
+        {
+            Assert.IsTrue(_grid.IsPositionInGrid(0, 0));
+            Assert.IsTrue(_grid.IsPositionInGrid(Vector2Int.zero));
+            
+            Assert.IsTrue(_grid.IsPositionInGrid(WIDTH - 1, 0));
+            Assert.IsTrue(_grid.IsPositionInGrid(new Vector2Int(WIDTH - 1, 0)));
+            
+            Assert.IsTrue(_grid.IsPositionInGrid(0, HEIGHT - 1));
+            Assert.IsTrue(_grid.IsPositionInGrid(new Vector2Int(0, HEIGHT - 1)));
+            
+            Assert.IsTrue(_grid.IsPositionInGrid(WIDTH - 1, HEIGHT - 1));
+            Assert.IsTrue(_grid.IsPositionInGrid(new Vector2Int(WIDTH - 1, HEIGHT - 1)));
+            
+            Assert.IsTrue(_grid.IsPositionInGrid(0, 0));
+            Assert.IsTrue(_grid.IsPositionInGrid(Vector2Int.zero));
+            
+            Assert.IsFalse(_grid.IsPositionInGrid(-1, -1));
+            Assert.IsFalse(_grid.IsPositionInGrid(new Vector2Int(-1, -1)));
+        }
+    }
+}
