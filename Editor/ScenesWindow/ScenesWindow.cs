bb0eff679a1c9d890ce22ff5bd7f27d07c342a5f
ImInfenix <duncan.sourdin@pm.me>
2022-04-13 13:23:09 +0200

    Fixed scenes window layouting

--- Editor/ScenesWindow/ScenesWindow.cs
+++ Editor/ScenesWindow/ScenesWindow.cs
@@ -12,7 +12,7 @@ namespace UniCorn
         private static string[] _existingScenesPaths;
         private static string[] _existingScenesNames;
 
-        private static readonly Vector2Int WINDOW_MARGIN_VECTOR = new(5, 5);
+        private static readonly Vector2Int WINDOW_MARGIN_VECTOR = new(0, 5);
         private static readonly Vector2Int BUTTON_SIZE = new(150, 20);
 
         private static Vector2 _scrollPosition;
@@ -60,23 +60,23 @@ namespace UniCorn
 
             Rect windowContentSize = new Rect(WINDOW_MARGIN_VECTOR, _scenesWindow.position.size - WINDOW_MARGIN_VECTOR * 2);
 
-            int maximumButtonPerRaw = Mathf.Max((int) _scenesWindow.position.width / (BUTTON_SIZE.x + WINDOW_MARGIN_VECTOR.x), 1);
+            int maximumButtonPerRaw = Mathf.Max((int) windowContentSize.width / (BUTTON_SIZE.x + WINDOW_MARGIN_VECTOR.x), 1);
 
             Vector2Int totalNumberOfButtons = new Vector2Int(maximumButtonPerRaw, _existingScenesNames.Length / maximumButtonPerRaw);
-            Rect viewRect = new Rect(Vector2.zero,
-                new Vector2(windowContentSize.width - WINDOW_MARGIN_VECTOR.x / 2, BUTTON_SIZE.y * totalNumberOfButtons.y + WINDOW_MARGIN_VECTOR.y * 2));
+            Rect viewRect = new Rect(-WINDOW_MARGIN_VECTOR / 2,
+                new Vector2(windowContentSize.width - WINDOW_MARGIN_VECTOR.x / 2f, BUTTON_SIZE.y * totalNumberOfButtons.y + WINDOW_MARGIN_VECTOR.y * 2));
 
             _scrollPosition = GUI.BeginScrollView(windowContentSize, _scrollPosition, viewRect);
+
+            float positionAvailableForButtons = viewRect.width - WINDOW_MARGIN_VECTOR.x * 2;
+            float positionAvailablePerButton = positionAvailableForButtons / maximumButtonPerRaw;
 
             for (int i = 0; i < _existingScenesNames.Length; i++)
             {
                 Vector2 positionInGridAsCell = new Vector2Int(i % maximumButtonPerRaw, i / maximumButtonPerRaw);
                 Vector2 positionInGrid = (BUTTON_SIZE + WINDOW_MARGIN_VECTOR) * positionInGridAsCell;
 
-                float positionAvailableForButtons = viewRect.width - WINDOW_MARGIN_VECTOR.x * 2;
-                float positionAvailablePerButton = positionAvailableForButtons / maximumButtonPerRaw;
-
-                positionInGrid.x = WINDOW_MARGIN_VECTOR.x / 2
+                positionInGrid.x = WINDOW_MARGIN_VECTOR.x / 2f
                                    + positionInGridAsCell.x * positionAvailablePerButton
                                    + (positionAvailablePerButton - BUTTON_SIZE.x) / 2;
 
