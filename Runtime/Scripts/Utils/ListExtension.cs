16c5049060646eabf1bf4b1062abc55fba921bd3
ImInfenix <46200328+ImInfenix@users.noreply.github.com>
2022-04-14 13:26:51 +0200

    Added tests for ListExtension

--- Runtime/Scripts/Utils/ListExtension.cs
+++ Runtime/Scripts/Utils/ListExtension.cs
@@ -5,6 +5,11 @@ namespace UniCorn.Utils
 {
     public static class ListExtension
     {
+        public static bool IsEmpty<T>(this List<T> list)
+        {
+            return list.Count == 0;
+        }
+
         public static T GetLastOrDefault<T>(this List<T> list)
         {
             if (list == null)
---
+++ Tests/Runtime/Utils/ListExtensionTest.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UniCorn.Utils;
+
+namespace UniCorn.Tests.Utils
+{
+    public class ListExtensionTest
+    {
+        [Test]
+        public void IsEmptyNullList()
+        {
+            List<int> list = null;
+            // ReSharper disable once ExpressionIsAlwaysNull
+            Assert.Throws<NullReferenceException>(() => list.IsEmpty());
+        }
+
+        [Test]
+        public void IsEmptyEmptyList()
+        {
+            List<int> list = new();
+            Assert.IsTrue(list.IsEmpty());
+        }
+
+        [Test]
+        public void IsEmptyNonEmptyList()
+        {
+            List<int> list = new() { 5 };
+            Assert.IsFalse(list.IsEmpty());
+        }
+
+        [Test]
+        public void GetLastOrDefaultNullList()
+        {
+            List<int> list = null;
+            // ReSharper disable once ExpressionIsAlwaysNull
+            Assert.Throws<NullReferenceException>(() => list.GetLastOrDefault());
+        }
+
+        [Test]
+        public void GetLastOrDefaultEmptyList()
+        {
+            List<int> list = new();
+            Assert.AreEqual(0, list.GetLastOrDefault());
+        }
+
+        [Test]
+        public void GetLastOrDefaultOneItemList()
+        {
+            List<int> list = new() { 5 };
+            Assert.AreEqual(5, list.GetLastOrDefault());
+        }
+
+        [Test]
+        public void GetLastOrDefaultMultipleItemsList()
+        {
+            List<int> list = new() { 5, 2, 8 };
+            Assert.AreEqual(8, list.GetLastOrDefault());
+        }
+    }
+}
