6a01ad46fa9caecbb0c18c16449ccde647333402
ImInfenix <duncan.sourdin@pm.me>
2022-04-14 13:12:07 +0200

    Created StringExtension

---
+++ Runtime/Scripts/Utils/StringExtension.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace UniCorn.Utils
+{
+    public static class StringExtension
+    {
+        public static bool IsEmpty<T>(this List<T> list)
+        {
+            return list.Count == 0;
+        }
+    }
+}
