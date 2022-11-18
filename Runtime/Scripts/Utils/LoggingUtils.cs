af7e477d4c58c55e2b42228fcfaa69f4a9235e2d
ImInfenix <46200328+ImInfenix@users.noreply.github.com>
2022-11-18 20:33:50 +0100

    Created LoggingUtils

---
+++ Runtime/Scripts/Utils/LoggingUtils.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace UniCorn.Utils
+{
+    public static class LoggingUtils
+    {
+        public static string ToVariableDisplayName(string variableName, object variable) => $"{variableName}: {variable}";
+        public static string ToVariableDisplayName(string variableName, GameObject variable)
+        {
+            return ToVariableDisplayName(variableName, variable ? variable.name : null);
+        }
+    }
+}
