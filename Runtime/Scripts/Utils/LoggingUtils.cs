397de85c0389cdac24e1c1a8ef0c18cae8bcc267
ImInfenix <46200328+ImInfenix@users.noreply.github.com>
2023-01-21 20:19:35 +0100

    Fix ToVariableDisplayName overload that should be for Components, not GameObjects

--- Runtime/Scripts/Utils/LoggingUtils.cs
+++ Runtime/Scripts/Utils/LoggingUtils.cs
@@ -5,7 +5,7 @@ namespace UniCorn.Utils
     public static class LoggingUtils
     {
         public static string ToVariableDisplayName(string variableName, object variable) => $"{variableName}: {variable}";
-        public static string ToVariableDisplayName(string variableName, GameObject variable)
+        public static string ToVariableDisplayName(string variableName, Component variable)
         {
             return ToVariableDisplayName(variableName, variable ? variable.name : null);
         }
