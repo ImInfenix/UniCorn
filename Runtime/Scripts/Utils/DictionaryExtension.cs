dcd1d1346f2aca680f49d6225ebcf33c56df9c2d
ImInfenix <46200328+ImInfenix@users.noreply.github.com>
2023-02-02 19:49:20 +0100

    Created RemoveAndRetrieveIfExist method for dictionaries

--- Runtime/Scripts/Utils/DictionaryExtension.cs
+++ Runtime/Scripts/Utils/DictionaryExtension.cs
@@ -2,16 +2,27 @@
 
 namespace UniCorn.Utils
 {
-    public static class DictionaryExtension
-    {
-        public static void AddIfDoesntExist<K, V>(this Dictionary<K, V> dictionary, K key, V value)
-        {
-            if (dictionary.ContainsKey(key))
-            {
-                return;
-            }
+	public static class DictionaryExtension
+	{
+		public static void AddIfDoesntExist<K, V>(this Dictionary<K, V> dictionary, K key, V value)
+		{
+			if (dictionary.ContainsKey(key))
+			{
+				return;
+			}
 
-            dictionary.Add(key, value);
-        }
-    }
+			dictionary.Add(key, value);
+		}
+
+		public static bool RemoveAndRetrieveIfExist<K, V>(this Dictionary<K, V> dictionary, K key, out V value)
+		{
+			if (!dictionary.TryGetValue(key, out value))
+			{
+				return false;
+			}
+
+			dictionary.Remove(key);
+			return true;
+		}
+	}
 }
