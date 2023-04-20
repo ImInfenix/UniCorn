6ba3603e95b7a7bc9473d812950a888643ea1e73
ImInfenix <46200328+ImInfenix@users.noreply.github.com>
2023-04-20 13:27:21 +0200

    Added AbstractDataService to load all assets with a specific key and allow to access them easily

---
+++ Runtime/Scripts/Core/AsynchronousLoading/AbstractDataService.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace UniCorn.Core.AsynchronousLoading
+{
+	public abstract class AbstractDataService<DATA_TYPE> : AbstractSynchronizedAsynchronouslyLoadedService where DATA_TYPE : ScriptableObject
+	{
+		private readonly List<DATA_TYPE> _loadedData = new();
+
+		protected abstract string DataAddressableKey { get; }
+
+		public IReadOnlyList<DATA_TYPE> LoadedData => _loadedData;
+
+		protected AbstractDataService(AbstractAsynchronouslyLoadedServiceSynchronizer synchronizer) : base(synchronizer)
+		{
+		}
+
+		protected override void InitializeInternal()
+		{
+			LoadAssetsAsync<DATA_TYPE>(DataAddressableKey, operationHandle =>
+			{
+				foreach (DATA_TYPE buildingBlueprintData in operationHandle.Result)
+				{
+					_loadedData.Add(buildingBlueprintData);
+				}
+			});
+		}
+
+		protected override void DisposeInternal()
+		{
+		}
+	}
+}
