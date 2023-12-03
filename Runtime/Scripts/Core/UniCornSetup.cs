492016559b4d5ef246b331ea7cd9315d71f3af81
ImInfenix <46200328+ImInfenix@users.noreply.github.com>
2023-12-03 20:02:38 +0100

    Added some basic dependency injection to UniCorn Standalone

--- Runtime/Scripts/Core/UniCornSetup.cs
+++ Runtime/Scripts/Core/UniCornSetup.cs
@@ -1,33 +1,65 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Collections.Generic;
+using UniCorn.Localization;
+using UniCorn.Navigation;
+using UnityEngine;
+using UnityEngine.InputSystem;
 
 namespace UniCorn.Core
 {
-    public class UniCornSetup : MonoBehaviour
-    {
-        private UniCornSetup _instance;
+	public class UniCornSetup : MonoBehaviour
+	{
+		[SerializeField] private IInputActionCollection _inputActionCollection;
+		[SerializeField] private LocalizationSettings _localizationSettings;
 
-        private void Awake()
-        {
-            if(_instance != null)
-            {
-                Destroy(gameObject);
-                Debug.LogWarning($"Multiple instance of UniCornSetup detected. Destroying {gameObject.name}");
-                return;
-            }
+		public static UniCornSetup Instance { get; private set; }
 
-            DontDestroyOnLoad(gameObject);
+		private ICoroutineHandler _coroutineHandler;
+		private Dictionary<Type, IService> _initializedServices = new();
 
-            Setup();
-        }
+		public ICoroutineHandler GetCoroutineHandler()
+		{
+			return _coroutineHandler;
+		}
 
-        private void Setup()
-        {
-            InitializeServices();
-        }
+		public T GetService<T>() where T : IService
+		{
+			return (T) _initializedServices[typeof(T)];
+		}
 
-        private void InitializeServices()
-        {
-            //TODO initialize services in case of a basic setup, when not using the zenject implementation
-        }
-    }
+		private void Awake()
+		{
+			if (Instance != null)
+			{
+				Destroy(gameObject);
+				Debug.LogWarning($"Multiple instance of UniCornSetup detected. Destroying {gameObject.name}");
+				return;
+			}
+
+			DontDestroyOnLoad(gameObject);
+
+			Instance = this;
+			Setup();
+		}
+
+		private void Setup()
+		{
+			_coroutineHandler = gameObject.AddComponent<UniCornMonoBehaviour>();
+			InitializeServices();
+		}
+
+		private void InitializeServices()
+		{
+			InitializeSingleService<NavigationService>();
+			// TODO initialize input service from user's code inherited type
+			//InitializeSingleService<InputService>(_inputActionCollection, GetService<NavigationService>());
+			InitializeSingleService<TranslationService>(_localizationSettings, _coroutineHandler);
+			InitializeSingleService<LocalizationSystemService>(_localizationSettings, GetService<TranslationService>());
+		}
+
+		private void InitializeSingleService<T>(params object[] args) where T : IService
+		{
+			_initializedServices[typeof(T)] = (IService) Activator.CreateInstance(typeof(T), args);
+		}
+	}
 }
