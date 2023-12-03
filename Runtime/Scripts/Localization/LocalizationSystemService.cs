8e1a90f6e58cd68749471274c5b303f897a05d45
ImInfenix <46200328+ImInfenix@users.noreply.github.com>
2023-12-03 20:00:58 +0100

    Renamed LocalizationSystem by LocalizationSystemService to make it more clear

---
+++ Runtime/Scripts/Localization/LocalizationSystemService.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace UniCorn.Localization
+{
+	public class LocalizationSystemService : AbstractLocalizationService
+	{
+		private readonly TranslationService _translationService;
+
+		public LocalizationSystemService(LocalizationSettings localizationSettings, TranslationService translationService) : base(localizationSettings)
+		{
+			_translationService = translationService;
+		}
+
+		public override void SwitchToLanguage(SystemLanguage newTargetedLanguage)
+		{
+			Assert.IsTrue(_localizationSettings.SupportedLanguages.Contains(newTargetedLanguage));
+
+			_translationService.SwitchToLanguage(newTargetedLanguage);
+		}
+	}
+}
