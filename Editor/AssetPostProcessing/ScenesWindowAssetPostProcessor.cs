e8e74c6d5019d2ae96540170088be05ea9ab8925
ImInfenix <46200328+ImInfenix@users.noreply.github.com>
2023-12-03 18:26:48 +0100

    Fixed ScenesWindow adding temporary test scenes from Unity Testing Framework when running tests

--- Editor/AssetPostProcessing/ScenesWindowAssetPostProcessor.cs
+++ Editor/AssetPostProcessing/ScenesWindowAssetPostProcessor.cs
@@ -1,26 +1,43 @@
-using System.Linq;
+using System.Collections.Generic;
 using UnityEditor;
 
 namespace UniCorn.Editor.AssetPostProcessing
 {
-    public class ScenesWindowAssetPostProcessor : AssetPostprocessor
-    {
-        private const string SCENE_FILES_EXTENSION = ".unity";
+	public class ScenesWindowAssetPostProcessor : AssetPostprocessor
+	{
+		private static void OnPostprocessAllAssets(string[] importedAssets, string[] deletedAssets, string[] movedAssets, string[] movedFromAssetPaths)
+		{
+			if (!EditorWindow.HasOpenInstances<ScenesWindow.ScenesWindow>())
+			{
+				return;
+			}
 
-        private static void OnPostprocessAllAssets(string[] importedAssets, string[] deletedAssets, string[] movedAssets, string[] movedFromAssetPaths)
-        {
-            if (!EditorWindow.HasOpenInstances<ScenesWindow.ScenesWindow>())
-            {
-                return;
-            }
+			List<string[]> arraysToTest = new List<string[]> {importedAssets, deletedAssets, movedAssets, movedFromAssetPaths};
 
-            if (importedAssets.Any(assetPath => assetPath.EndsWith(SCENE_FILES_EXTENSION)) ||
-                deletedAssets.Any(assetPath => assetPath.EndsWith(SCENE_FILES_EXTENSION)) ||
-                movedAssets.Any(assetPath => assetPath.EndsWith(SCENE_FILES_EXTENSION)) ||
-                movedFromAssetPaths.Any(assetPath => assetPath.EndsWith(SCENE_FILES_EXTENSION)))
-            {
-                ScenesWindow.ScenesWindow.UpdateWindowContent();
-            }
-        }
-    }
+			bool shouldUpdate = false;
+			foreach (string[] updatedAssets in arraysToTest)
+			{
+				foreach (string assetPath in updatedAssets)
+				{
+					if (!ScenesWindow.ScenesWindow.ShouldSceneBeDisplayed(assetPath))
+					{
+						continue;
+					}
+
+					shouldUpdate = true;
+					break;
+				}
+
+				if (shouldUpdate)
+				{
+					break;
+				}
+			}
+
+			if (shouldUpdate)
+			{
+				ScenesWindow.ScenesWindow.UpdateWindowContent();
+			}
+		}
+	}
 }
--- Editor/ScenesWindow/ScenesWindow.cs
+++ Editor/ScenesWindow/ScenesWindow.cs
@@ -7,104 +7,123 @@ using UnityEngine;
 
 namespace UniCorn.Editor.ScenesWindow
 {
-    public class ScenesWindow : EditorWindow
-    {
-        private static ScenesWindow _scenesWindow;
+	public class ScenesWindow : EditorWindow
+	{
+		private static ScenesWindow _scenesWindow;
 
-        private static List<string> _existingScenesPaths = new();
-        private static List<string> _existingScenesNames = new();
+		private static readonly List<string> _existingScenesPaths = new();
+		private static readonly List<string> _existingScenesNames = new();
 
-        private static readonly Vector2Int WINDOW_MARGIN_VECTOR = new(0, 5);
-        private static readonly Vector2Int BUTTON_SIZE = new(150, 20);
+		private static readonly Vector2Int WINDOW_MARGIN_VECTOR = new(0, 5);
+		private static readonly Vector2Int BUTTON_SIZE = new(150, 20);
 
-        private static Vector2 _scrollPosition;
+		private static Vector2 _scrollPosition;
 
-        [MenuItem("UniCorn/Scenes Window", priority = 100)]
-        public static void OpenWindow()
-        {
-            _scenesWindow = GetWindow<ScenesWindow>();
-            _scenesWindow.titleContent = new GUIContent("Scenes Window");
-            _scenesWindow.maximized = true;
-            _scenesWindow.Show();
-        }
+		[MenuItem("UniCorn/Scenes Window", priority = 100)]
+		public static void OpenWindow()
+		{
+			_scenesWindow = GetWindow<ScenesWindow>();
+			_scenesWindow.titleContent = new GUIContent("Scenes Window");
+			_scenesWindow.maximized = true;
+			_scenesWindow.Show();
+		}
 
-        public static void UpdateWindowContent()
-        {
-            string[] existingScenesGuids = AssetDatabase.FindAssets($"t:scene", new[] {"Assets"});
+		public static void UpdateWindowContent()
+		{
+			string[] existingScenesGuids = AssetDatabase.FindAssets($"t:scene", new[] {"Assets"});
 
-            _existingScenesPaths.Clear();
-            _existingScenesNames.Clear();
+			_existingScenesPaths.Clear();
+			_existingScenesNames.Clear();
 
-            for (int i = 0; i < existingScenesGuids.Length; i++)
-            {
-                string sceneGuid = existingScenesGuids[i];
-                string sceneAssetPath = AssetDatabase.GUIDToAssetPath(sceneGuid);
+			for (int i = 0; i < existingScenesGuids.Length; i++)
+			{
+				string sceneGuid = existingScenesGuids[i];
+				string sceneAssetPath = AssetDatabase.GUIDToAssetPath(sceneGuid);
 
-                if (sceneAssetPath.ToLower().Contains("template"))
-                {
-                    continue;
-                }
+				if (!ShouldSceneBeDisplayed(sceneAssetPath))
+				{
+					continue;
+				}
 
-                _existingScenesPaths.Add(sceneAssetPath);
-                _existingScenesNames.Add(Path.GetFileNameWithoutExtension(_existingScenesPaths[i]));
+				_existingScenesPaths.Add(sceneAssetPath);
+				string sceneName = Path.GetFileNameWithoutExtension(_existingScenesPaths.GetLast());
+				_existingScenesNames.Add(sceneName);
+			}
+		}
 
-            }
-        }
+		public static bool ShouldSceneBeDisplayed(string sceneAssetPath)
+		{
+			const string SCENE_FILES_EXTENSION = ".unity";
+			const string TEST_SCENE_NAME_START = "InitTestScene";
 
-        private void OnGUI()
-        {
-            if (_scenesWindow == null)
-            {
-                _scenesWindow = GetWindow<ScenesWindow>();
-            }
+			string assetFileName = Path.GetFileName(sceneAssetPath);
+			if (Path.GetExtension(assetFileName) != SCENE_FILES_EXTENSION)
+			{
+				return false;
+			}
 
-            if (_existingScenesNames.IsEmpty())
-            {
-                UpdateWindowContent();
+			if (Path.GetFileNameWithoutExtension(assetFileName).StartsWith(TEST_SCENE_NAME_START))
+			{
+				return false;
+			}
 
-                if (_existingScenesNames == null)
-                {
-                    return;
-                }
-            }
+			return true;
+		}
 
-            Rect windowContentSize = new Rect(WINDOW_MARGIN_VECTOR, _scenesWindow.position.size - WINDOW_MARGIN_VECTOR * 2);
+		private void OnGUI()
+		{
+			if (_scenesWindow == null)
+			{
+				_scenesWindow = GetWindow<ScenesWindow>();
+			}
 
-            int maximumButtonPerRaw = Mathf.Max((int) windowContentSize.width / (BUTTON_SIZE.x + WINDOW_MARGIN_VECTOR.x), 1);
+			if (_existingScenesNames.IsEmpty())
+			{
+				UpdateWindowContent();
 
-            Vector2Int totalNumberOfButtons = new Vector2Int(maximumButtonPerRaw, _existingScenesNames.Count / maximumButtonPerRaw);
-            Rect viewRect = new Rect(-WINDOW_MARGIN_VECTOR / 2,
-                new Vector2(windowContentSize.width - WINDOW_MARGIN_VECTOR.x / 2f, BUTTON_SIZE.y * totalNumberOfButtons.y + WINDOW_MARGIN_VECTOR.y * 2));
+				if (_existingScenesNames == null)
+				{
+					return;
+				}
+			}
 
-            _scrollPosition = GUI.BeginScrollView(windowContentSize, _scrollPosition, viewRect);
+			Rect windowContentSize = new Rect(WINDOW_MARGIN_VECTOR, _scenesWindow.position.size - WINDOW_MARGIN_VECTOR * 2);
 
-            float positionAvailableForButtons = viewRect.width - WINDOW_MARGIN_VECTOR.x * 2;
-            float positionAvailablePerButton = positionAvailableForButtons / maximumButtonPerRaw;
+			int maximumButtonPerRaw = Mathf.Max((int) windowContentSize.width / (BUTTON_SIZE.x + WINDOW_MARGIN_VECTOR.x), 1);
 
-            for (int i = 0; i < _existingScenesNames.Count; i++)
-            {
-                Vector2 positionInGridAsCell = new Vector2Int(i % maximumButtonPerRaw, i / maximumButtonPerRaw);
-                Vector2 positionInGrid = (BUTTON_SIZE + WINDOW_MARGIN_VECTOR) * positionInGridAsCell;
+			Vector2Int totalNumberOfButtons = new Vector2Int(maximumButtonPerRaw, _existingScenesNames.Count / maximumButtonPerRaw);
+			Rect viewRect = new Rect(-WINDOW_MARGIN_VECTOR / 2,
+				new Vector2(windowContentSize.width - WINDOW_MARGIN_VECTOR.x / 2f, BUTTON_SIZE.y * totalNumberOfButtons.y + WINDOW_MARGIN_VECTOR.y * 2));
 
-                positionInGrid.x = WINDOW_MARGIN_VECTOR.x / 2f
-                                   + positionInGridAsCell.x * positionAvailablePerButton
-                                   + (positionAvailablePerButton - BUTTON_SIZE.x) / 2;
+			_scrollPosition = GUI.BeginScrollView(windowContentSize, _scrollPosition, viewRect);
 
-                if (GUI.Button(new Rect(positionInGrid, BUTTON_SIZE), _existingScenesNames[i]))
-                {
-                    LoadScene(_existingScenesPaths[i]);
-                }
-            }
+			float positionAvailableForButtons = viewRect.width - WINDOW_MARGIN_VECTOR.x * 2;
+			float positionAvailablePerButton = positionAvailableForButtons / maximumButtonPerRaw;
 
-            GUI.EndScrollView();
-        }
+			for (int i = 0; i < _existingScenesNames.Count; i++)
+			{
+				Vector2 positionInGridAsCell = new Vector2Int(i % maximumButtonPerRaw, i / maximumButtonPerRaw);
+				Vector2 positionInGrid = (BUTTON_SIZE + WINDOW_MARGIN_VECTOR) * positionInGridAsCell;
 
-        private void LoadScene(string scenePath)
-        {
-            if (EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo())
-            {
-                EditorSceneManager.OpenScene(scenePath);
-            }
-        }
-    }
+				positionInGrid.x = WINDOW_MARGIN_VECTOR.x / 2f
+				                   + positionInGridAsCell.x * positionAvailablePerButton
+				                   + (positionAvailablePerButton - BUTTON_SIZE.x) / 2;
+
+				if (GUI.Button(new Rect(positionInGrid, BUTTON_SIZE), _existingScenesNames[i]))
+				{
+					LoadScene(_existingScenesPaths[i]);
+				}
+			}
+
+			GUI.EndScrollView();
+		}
+
+		private void LoadScene(string scenePath)
+		{
+			if (EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo())
+			{
+				EditorSceneManager.OpenScene(scenePath);
+			}
+		}
+	}
 }
