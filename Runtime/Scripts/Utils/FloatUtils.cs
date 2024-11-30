cc2b744220cfa8a37b7ec2afef855241db0e3ebb
ImInfenix <46200328+ImInfenix@users.noreply.github.com>
2024-11-30 01:53:03 +0100

    Added FloatUtils.AreAlmostEqual

---
+++ Runtime/Scripts/Utils/FloatUtils.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace UniCorn.Utils
+{
+    public static class FloatUtils
+    {
+        public const float PRECISION = 0.001f;
+
+        public static bool AreAlmostEqual(float value1, float value2)
+        {
+            return Mathf.Abs(value1 - value2) < PRECISION;
+        }
+    }
+}
---
+++ Tests/Runtime/Utils/FloatUtilsTest.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using UniCorn.Utils;
+
+namespace UniCorn.Tests.Utils
+{
+    public class FloatUtilsTest
+    {
+        [TestCase(0, 0, ExpectedResult = true)]
+        [TestCase(0, 1, ExpectedResult = false)]
+        [TestCase(0, FloatUtils.PRECISION * 0.1f, ExpectedResult = true)]
+        [TestCase(0.3f, 0.1f + 0.2f, ExpectedResult = true)]
+        [TestCase(0.3f, 0.3000005f, ExpectedResult = true)]
+        public bool AreAlmostEqualTest(params float[] elements)
+        {
+            Assert.AreEqual(2, elements.Length);
+            return FloatUtils.AreAlmostEqual(elements[0], elements[1]);
+        }
+    }
+}
