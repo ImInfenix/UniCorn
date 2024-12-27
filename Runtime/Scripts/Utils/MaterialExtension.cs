0938997244132f857a9fadde1bd0ea837957449d
ImInfenix <46200328+ImInfenix@users.noreply.github.com>
2024-12-27 17:56:56 +0100

    Added MaterialExtension to set and retrieve Diffuse texture on materials

---
+++ Runtime/Scripts/Utils/MaterialExtension.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace UniCorn.Utils
+{
+    public static class MaterialExtension
+    {
+        private static readonly int _baseMap = Shader.PropertyToID("_BaseMap");
+
+        public static Texture GetDiffuse(this Material material)
+        {
+            return material.GetTexture(_baseMap);
+        }
+
+        public static void SetDiffuse(this Material material, Texture texture)
+        {
+            material.SetTexture(_baseMap, texture);
+        }
+    }
+}
