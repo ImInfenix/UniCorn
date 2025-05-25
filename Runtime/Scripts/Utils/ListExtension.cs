345e51be3bd976102c728d4ffdf85d1b854afe53
ImInfenix <46200328+ImInfenix@users.noreply.github.com>
2025-05-25 19:03:29 +0200

    Use IReadOnlyList instead of List where possible for list extensions

--- Runtime/Scripts/Utils/ListExtension.cs
+++ Runtime/Scripts/Utils/ListExtension.cs
@@ -1,22 +1,24 @@
 ﻿using System.Collections.Generic;
+using UnityEngine.Assertions;
 
 namespace UniCorn.Utils
 {
-	public static class ListExtension
-	{
-		public static bool IsEmpty<T>(this IReadOnlyCollection<T> list)
-		{
-			return list.Count == 0;
-		}
+    public static class ListExtension
+    {
+        public static bool IsEmpty<T>(this IReadOnlyCollection<T> list)
+        {
+            return list.Count == 0;
+        }
 
-		public static T GetLast<T>(this List<T> list)
-		{
-			return list[^1];
-		}
+        public static T GetLast<T>(this IReadOnlyList<T> list)
+        {
+            Assert.IsTrue(!list.IsEmpty());
+            return list[^1];
+        }
 
-		public static T GetLastOrDefault<T>(this List<T> list)
-		{
-			return list.Count == 0 ? default : list[^1];
-		}
-	}
+        public static T GetLastOrDefault<T>(this IReadOnlyList<T> list)
+        {
+            return list.IsEmpty() ? default : list[^1];
+        }
+    }
 }
--- Tests/Editor/Utils/ListExtensionTest.cs
+++ Tests/Editor/Utils/ListExtensionTest.cs
@@ -2,61 +2,62 @@
 using System.Collections.Generic;
 using NUnit.Framework;
 using UniCorn.Utils;
+using AssertionException = UnityEngine.Assertions.AssertionException;
 
 namespace UniCorn.Tests.Editor.Utils
 {
-	public class ListExtensionTest
-	{
-		[TestCase(typeof(NullReferenceException))]
-		public void IsEmptyNullListTest(Type type)
-		{
-			Assert.Throws(type, () => ((List<int>) null).IsEmpty());
-		}
+    public class ListExtensionTest
+    {
+        [TestCase(typeof(NullReferenceException))]
+        public void IsEmptyNullListTest(Type type)
+        {
+            Assert.Throws(type, () => ((List<int>)null).IsEmpty());
+        }
 
-		[TestCase(ExpectedResult = true)]
-		[TestCase(5, ExpectedResult = false)]
-		[TestCase(5, 2, ExpectedResult = false)]
-		[TestCase(5, 2, 8, ExpectedResult = false)]
-		public bool IsEmptyTest(params int[] listContent)
-		{
-			return new List<int>(listContent).IsEmpty();
-		}
+        [TestCase(ExpectedResult = true)]
+        [TestCase(5, ExpectedResult = false)]
+        [TestCase(5, 2, ExpectedResult = false)]
+        [TestCase(5, 2, 8, ExpectedResult = false)]
+        public bool IsEmptyTest(params int[] listContent)
+        {
+            return new List<int>(listContent).IsEmpty();
+        }
 
-		[TestCase(false, typeof(NullReferenceException))]
-		[TestCase(true, typeof(ArgumentOutOfRangeException))]
-		public void GetLastNullListTest(bool initialize, Type type)
-		{
-			List<int> list = null;
+        [TestCase(false, typeof(NullReferenceException))]
+        [TestCase(true, typeof(AssertionException))]
+        public void GetLastNullListTest(bool initialize, Type type)
+        {
+            List<int> list = null;
 
-			if (initialize)
-			{
-				list = new();
-			}
+            if (initialize)
+            {
+                list = new();
+            }
 
-			Assert.Throws(type, () => list.GetLast());
-		}
+            Assert.Throws(type, () => list.GetLast());
+        }
 
-		[TestCase(5, ExpectedResult = 5)]
-		[TestCase(5, 2, ExpectedResult = 2)]
-		[TestCase(5, 2, 8, ExpectedResult = 8)]
-		public int GetLastTest(params int[] listContent)
-		{
-			return new List<int>(listContent).GetLast();
-		}
+        [TestCase(5, ExpectedResult = 5)]
+        [TestCase(5, 2, ExpectedResult = 2)]
+        [TestCase(5, 2, 8, ExpectedResult = 8)]
+        public int GetLastTest(params int[] listContent)
+        {
+            return new List<int>(listContent).GetLast();
+        }
 
-		[TestCase(typeof(NullReferenceException))]
-		public void GetLastOrDefaultNullListTest(Type type)
-		{
-			Assert.Throws(type, () => ((List<int>) null).GetLastOrDefault());
-		}
+        [TestCase(typeof(NullReferenceException))]
+        public void GetLastOrDefaultNullListTest(Type type)
+        {
+            Assert.Throws(type, () => ((List<int>)null).GetLastOrDefault());
+        }
 
-		[TestCase(ExpectedResult = default(int))]
-		[TestCase(5, ExpectedResult = 5)]
-		[TestCase(5, 2, ExpectedResult = 2)]
-		[TestCase(5, 2, 8, ExpectedResult = 8)]
-		public int GetLastOrDefaultTest(params int[] listContent)
-		{
-			return new List<int>(listContent).GetLastOrDefault();
-		}
-	}
+        [TestCase(ExpectedResult = default(int))]
+        [TestCase(5, ExpectedResult = 5)]
+        [TestCase(5, 2, ExpectedResult = 2)]
+        [TestCase(5, 2, 8, ExpectedResult = 8)]
+        public int GetLastOrDefaultTest(params int[] listContent)
+        {
+            return new List<int>(listContent).GetLastOrDefault();
+        }
+    }
 }
