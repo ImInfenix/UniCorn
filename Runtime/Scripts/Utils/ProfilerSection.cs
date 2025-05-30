049883d296d06be8c7986d135c213cd0d7e179e7
ImInfenix <46200328+ImInfenix@users.noreply.github.com>
2025-05-30 18:17:36 +0200

    Added ProfilerSection to make performance tracking easier

---
+++ Runtime/Scripts/Utils/ProfilerSection.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using UnityEngine.Profiling;
+
+namespace UniCorn.Utils
+{
+    public class ProfilerSection : IDisposable
+    {
+        public ProfilerSection(object objectBeingTracked) : this(objectBeingTracked.GetType().ToString())
+        {
+        }
+
+        public ProfilerSection(string sectionName)
+        {
+            Profiler.BeginSample(sectionName);
+        }
+
+        public void Dispose()
+        {
+            Profiler.EndSample();
+        }
+    }
+}
