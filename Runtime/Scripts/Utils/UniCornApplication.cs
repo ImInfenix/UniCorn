739273370f0db757e9af8eee652eb5eb17cf90de
ImInfenix <46200328+ImInfenix@users.noreply.github.com>
2025-05-31 08:36:23 +0200

    Added UniCornApplication to have an easy to use util to manipulate application without caller knowing editor context

---
+++ Runtime/Scripts/Utils/UniCornApplication.cs
@@ -0,0 +1,19 @@
+﻿#if UNITY_EDITOR
+using UnityEditor;
+#endif
+using UnityEngine;
+
+namespace UniCorn.Utils
+{
+    public static class UniCornApplication
+    {
+        public static void Quit()
+        {
+#if UNITY_EDITOR
+            EditorApplication.ExitPlaymode();
+#else
+            Application.Quit();
+#endif
+        }
+    }
+}
