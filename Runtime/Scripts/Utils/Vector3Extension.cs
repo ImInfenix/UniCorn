4596c4fd34ad8d7463e65963e5c6c9b8ba2ea78b
ImInfenix <46200328+ImInfenix@users.noreply.github.com>
2025-06-02 12:52:44 +0200

    Added Vector3 extension method XZ to convert into a Vector2

---
+++ Runtime/Scripts/Utils/Vector3Extension.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace UniCorn.Utils
+{
+    public static class Vector3Extension
+    {
+        public static Vector2 XZ(this Vector3 vector3)
+        {
+            return new Vector2(vector3.x, vector3.z);
+        }
+    }
+}
---
+++ Tests/Editor/Utils/Vector3ExtensionTest.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using UniCorn.Utils;
+using UnityEngine;
+
+namespace UniCorn.Tests.Editor.Utils
+{
+    public class Vector3ExtensionTest
+    {
+        [TestCase(0, 0.5f, 0, 0, 0, ExpectedResult = true)]
+        [TestCase(0, 0.5f, 1, 0, 1, ExpectedResult = true)]
+        [TestCase(1, 0.5f, 0, 1, 0, ExpectedResult = true)]
+        [TestCase(1, 0.5f, 1, 1, 1, ExpectedResult = true)]
+        public bool XZTest(params float[] elements)
+        {
+            Assert.AreEqual(elements.Length, 5);
+            Vector3 input = new Vector3(elements[0], elements[1], elements[2]);
+            Vector2 expectedOutput = new Vector2(elements[3], elements[4]);
+            return input.XZ() == expectedOutput;
+        }
+    }
+}
