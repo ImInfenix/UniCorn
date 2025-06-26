c9fa21e3fada7daddcc82f2fd04ad1e3e3bb4743
ImInfenix <46200328+ImInfenix@users.noreply.github.com>
2025-06-26 21:05:06 +0200

    Added a basic AABB struct

---
+++ Runtime/Scripts/Structures/AABB.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace UniCorn.Structures
+{
+    public struct AABB
+    {
+        public Vector2 Min { get; set; }
+        public Vector2 Max { get; set; }
+
+        public Vector2 Center => (Min + Max) / 2f;
+        public Vector2 Size => Max - Min;
+    }
+}
---
+++ Tests/Editor/Structures/AABBTest.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+using UniCorn.Structures;
+using UnityEngine;
+
+namespace UniCorn.Tests.Editor.Structures
+{
+    public class AABBTest
+    {
+        [Test]
+        public void SizeZero()
+        {
+            AABB aabb = new AABB() { Min = Vector2.zero, Max = Vector2.zero };
+            Assert.AreEqual(Vector2.zero, aabb.Center);
+            Assert.AreEqual(Vector2.zero, aabb.Size);
+        }
+
+        [Test]
+        public void SizeOne()
+        {
+            AABB aabb = new AABB() { Min = Vector2.zero, Max = Vector2.one };
+            Assert.AreEqual(Vector2.one / 2f, aabb.Center);
+            Assert.AreEqual(Vector2.one, aabb.Size);
+        }
+
+        [Test]
+        public void SizeMinusOne()
+        {
+            AABB aabb = new AABB() { Min = Vector2.zero, Max = Vector2.one * -1f };
+            Assert.AreEqual(Vector2.one / 2f * -1f, aabb.Center);
+            Assert.AreEqual(Vector2.one * -1f, aabb.Size);
+        }
+    }
+}
