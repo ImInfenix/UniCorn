374215bb70aaf611a632d827f6b5fbc5c19544a3
ImInfenix <46200328+ImInfenix@users.noreply.github.com>
2025-11-15 15:59:23 +0100

    Added RectUtils

---
+++ Runtime/Scripts/Utils/RectUtils.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.ResourceLocations;
+
+namespace UniCorn.Utils
+{
+    public static class RectUtils
+    {
+        public static Rect FromCenterAndSize(Vector2 center, Vector2 size)
+        {
+            return new Rect(center - size / 2f, size);
+        }
+
+        public static bool Contains(this Rect rect, Rect other)
+        {
+            return rect.Contains(other.position) && rect.Contains(other.position + other.size);
+        }
+    }
+}
---
+++ Tests/Editor/Utils/RectUtilsTest.cs
@@ -0,0 +1,41 @@
+﻿using NUnit.Framework;
+using UniCorn.Utils;
+using UnityEngine;
+
+namespace UniCorn.Tests.Editor.Utils
+{
+    public class RectUtilsTest
+    {
+        [TestCase(0, 0, 1, 1, -0.5f, -0.5f)]
+        [TestCase(1, 1, 2, 2, 0, 0)]
+        [TestCase(-1, -1, 2, 2, -2, -2)]
+        public void FromCenterAndSize(params float[] elements)
+        {
+            Assert.AreEqual(6, elements.Length);
+            Vector2 center = new Vector2(elements[0], elements[1]);
+            Vector2 size = new Vector2(elements[2], elements[3]);
+            Vector2 expectedPosition = new Vector2(elements[4], elements[5]);
+            Rect rect = RectUtils.FromCenterAndSize(center, size);
+            Assert.AreEqual(expectedPosition, rect.position);
+        }
+
+        [TestCase(0, 0, 1, 1, 0, 0, 1, 1, ExpectedResult = false)]
+        [TestCase(0, 0, 5, 5, 2, 2, 1, 1, ExpectedResult = true)]
+        [TestCase(0, 0, 1, 1, 0.5f, 0, 1, 1, ExpectedResult = false)]
+        [TestCase(0, 0, 1, 1, -0.5f, 0, 1, 1, ExpectedResult = false)]
+        [TestCase(0, 0, 1, 1, 0, 0.5f, 1, 1, ExpectedResult = false)]
+        [TestCase(0, 0, 1, 1, 0, -0.5f, 1, 1, ExpectedResult = false)]
+        public bool ContainsOtherRect(params float[] elements)
+        {
+            Vector2 containerPosition = new Vector2(elements[0], elements[1]);
+            Vector2 containerSize = new Vector2(elements[2], elements[3]);
+            Rect container = new Rect(containerPosition, containerSize);
+
+            Vector2 otherPosition = new Vector2(elements[4], elements[5]);
+            Vector2 otherSize = new Vector2(elements[6], elements[7]);
+            Rect other = new Rect(otherPosition, otherSize);
+
+            return container.Contains(other);
+        }
+    }
+}
